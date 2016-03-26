38de65e20e76c3eaf8d7894a725c27d130369b84
MilindPathiyal <milindpatiyal@gmail.com>
2016-03-26 10:46:39 -0700

    grid stuff update

--- Hackathon/Assets/Grid.cs
+++ Hackathon/Assets/Grid.cs
@@ -39,7 +39,8 @@ public class Grid : MonoBehaviour {
 				o.transform.position = new Vector2 (col, row);
 				p.transform.position = new Vector2 (col, row);
 				tileGrid[row, col] = o;
-				if (updateColorGrid) {
+				if (updateColorGrid) 
+				{
 					
 					tileGrid[row, col] = p;
 				}
@@ -61,7 +62,7 @@ public class Grid : MonoBehaviour {
 	}
 */
 		
-	public void bool updateColorGrid(int row, int col)
+	public void updateColorGrid(int row, int col)
 	{
 		grid [row, col] = !grid [row, col];
 	}
