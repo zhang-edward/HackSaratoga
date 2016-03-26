275ad2ba4a42ccc7f60fa4e5704030a9e99f32de
Edward Zhang <edwardz123@gmail.com>
2016-03-26 09:21:17 -0700

    Initial commit

---
+++ Hackathon/Assets/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	private int a;
+
+	// Use this for initialization
+	void Start () {
+		a = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
